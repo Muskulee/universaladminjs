9a973da6fc40920361e1d6e551c8e09ef2e739f6
Muskulee <muskulee@gmail.com>
2024-03-05 19:04:44 +0100

    modified all urls

--- app/_lib/api/config/urls.js
+++ app/_lib/api/config/urls.js
@@ -1,2 +1,2 @@
-export const apiURL = process.env.API_URL;
-export const webURL = process.env.WEB_URL;
+export const apiURL = process.env.NEXT_PUBLIC_API_URL;
+export const webURL = process.env.NEXT_WEB_URL;
